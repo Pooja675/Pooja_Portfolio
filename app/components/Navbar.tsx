dc079839d5c81f1aa3c390f828ecad430e6db1cf
Pooja675 <poojasinha675@gmail.com>
2025-01-31 16:59:27 +0530

    completed navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,122 @@
-import React from 'react'
+"use client"
 
-type Props = {}
+import React, { useCallback, useEffect, useState } from "react";
+import cn from "classnames";
 
-const Navbar = (props: Props) => {
+const Navbar = () => {
+
+    const [activeSection, setActiveSection] = useState("home")
+    const [isOpen, setIsOpen] = useState(false)
+
+    useEffect(() => {
+        setActiveSection("home")
+    },[])
+
+    const scrollToSection = useCallback((sectionId:string) => {
+            const section = document.getElementById(sectionId);
+            if(section){
+                section.scrollIntoView({
+                    behavior: "smooth",
+                })
+            }
+    },[])
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="fixed top-12 right-6 mx-auto flex flex-col gap-2.5 items-end z-20 md:right-auto md:left-1/2 md:-translate-x-1/2">
+      <button className="bg-background card-shadow p-3 md:hidden rounded" onClick={() => setIsOpen((prevVal) => !prevVal)}>
+        <img  className= "block dark:hidden" src="/menu_icon_light.svg" alt="menu icon" />
+        <img  className= "hidden dark:block" src="/menu_icon_dark.svg" alt="menu icon" />
+      </button>
+      <nav className={cn("bg-background card-shadow p-3 rounded md:block duration-300 ease-in-out", {
+        "opacity-100": isOpen,
+        "opacity-0 md:opacity-100":!isOpen,
+      })}>
+        <ul className="flex flex-col items-center gap-4 text-lg font-normal md:flex-row">
+            <li>
+                <div className={cn("rounded p-1 duration-300 ease-in-out",{
+                    "bg-primary text-white": activeSection === "home"
+                })} 
+                onClick={() => {
+                    setActiveSection("home")
+                    scrollToSection("home")
+                    setIsOpen(false)
+                }}
+                >
+                    Home
+                </div>
+            </li>
+            <li>
+                <div className={cn("rounded p-1 duration-300 ease-in-out",{
+                    "bg-primary text-white": activeSection === "skills"
+                })} 
+                onClick={() => {
+                    setActiveSection("skills")
+                    scrollToSection("skills")
+                    setIsOpen(false)
+                }}
+                >
+                    Skills
+                </div>
+            </li>
+            <li>
+                <div className={cn("rounded p-1 duration-300 ease-in-out",{
+                    "bg-primary text-white": activeSection === "projects"
+                })} 
+                onClick={() => {
+                    setActiveSection("projects")
+                    scrollToSection("projects")
+                    setIsOpen(false)
+                }}
+                >
+                    Projects
+                </div>
+            </li>
+            <li>
+                <div className={cn("rounded p-1 duration-300 ease-in-out",{
+                    "bg-primary text-white": activeSection === "education"
+                })} 
+                onClick={() => {
+                    setActiveSection("education")
+                    scrollToSection("education")
+                    setIsOpen(false)
+                }}
+                >
+                    Education
+                </div>
+            </li>
 
-export default Navbar
+            <li>
+                <div className={cn("rounded p-1 duration-300 ease-in-out",{
+                    "bg-primary text-white": activeSection === "experience"
+                })} 
+                onClick={() => {
+                    setActiveSection("experience")
+                    scrollToSection("experience")
+                    setIsOpen(false)
+                }}
+                >
+                    Experience
+                </div>
+            </li>
+
+            <li>
+                <div className={cn("rounded p-1 duration-300 ease-in-out",{
+                    "bg-primary text-white": activeSection === "contact"
+                })} 
+                onClick={() => {
+                    setActiveSection("contact")
+                    scrollToSection("contact")
+                    setIsOpen(false)
+                }}
+                >
+                    Contact
+                </div>
+            </li>
+            
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-  <h1>Hello World!!</h1>
+    <div className="relative overflow-clip" id="home">
+            <Navbar/>
+
+    </div>
   );
 }
