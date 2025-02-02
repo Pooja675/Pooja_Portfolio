eaa6bea6e57b1dea289ef46ded30994c780db80d
Pooja675 <poojasinha675@gmail.com>
2025-02-02 19:46:11 +0530

    Changes done

--- app/components/Reveal.tsx
+++ app/components/Reveal.tsx
@@ -12,6 +12,7 @@ type Props = React.PropsWithChildren<{
 const Reveal = ({
   children,
   initialX = 0,
+  initialY = 0,
   duration = 1,
   delay = 0,
 }: Props) => {
@@ -32,7 +33,7 @@ const Reveal = ({
     ref={ref}
       initial="hidden"
       variants={{
-        hidden: { opacity: 0, x: initialX, y: initialX },
+        hidden: { opacity: 0, x: initialX, y: initialY },
         visible: { opacity: 1, x: 0, y: 0 },
       }}
       transition={{ duration, delay }}
